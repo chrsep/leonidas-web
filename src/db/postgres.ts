50cc32254316733f2044b7dfd2822f5b838e46a7
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-25 23:08:30 +0700

    add docker, env and postgres

---
+++ src/db/postgres.ts
@@ -0,0 +1,9 @@
+import ServerlessClient from "serverless-postgres"
+
+const client = new ServerlessClient({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
