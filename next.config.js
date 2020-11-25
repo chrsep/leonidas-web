35979dd1c670f062cdcd3e712496aaa303148fd2
Chrisando Pramudhita <chrsep@protonmail.com>
2020-11-25 23:58:03 +0700

    fix pwwa.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withPWA = require('next-pwa')
 
-module.exports = withPWA({pwa: {disable: process.env.NODE_ENV === 'development'}}, {
+module.exports = withPWA({
+    pwa: {
+        disable: process.env.NODE_ENV === 'development',
+        dest: "public",
+    }
+}, {
     experimental: {
         modern: true,
     },
