dd1627235144bb41a763feadb3d3017b652a37c0
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-02-26 16:29:55 +0700

    move back to nextjs.
    for nivo charts and react's ecosystem.

--- src/lib/auth.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcryptjs"
-
-const saltRounds = 10
-
-export const validatePassword = (password, hashPassword) =>
-  bcrypt.compare(password, hashPassword)
-
-export const hashPassword = (password) => bcrypt.hash(password, saltRounds)
--- src/routes/auth/__layout.svelte
+++
@@ -1,7 +0,0 @@
-<div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-md">
-    <div class="bg-white py-8 px-4 sm:rounded-lg sm:px-10">
-      <slot />
-    </div>
-  </div>
-</div>
