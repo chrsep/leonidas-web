aeb8339e84b711957476ebbaaf1d9d32140e6dcc
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-02-26 17:43:40 +0700

    add preact.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const withPreact = require("next-plugin-preact");
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   experimental: {
     outputStandalone: true,
   },
+  swcMinify: true,
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPreact(nextConfig)
