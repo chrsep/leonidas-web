04793cf70bae51d5bba5ddef647283267ef37be8
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-03-07 12:33:16 +0700

    add CSP.

--- next.config.js
+++ next.config.js
@@ -12,6 +12,36 @@ const withPWA = [
 
 const plugins = [require("next-plugin-preact"), withPWA]
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self';
+  font-src 'self';  
+`
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+]
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   experimental: {
@@ -21,6 +51,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["images.unsplash.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ]
   },
 }
 
