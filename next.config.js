2586355a1c7fc0e8ed8ac3d14bee5d719acf1161
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-03-07 13:22:26 +0700

    simplify next config.

--- next.config.js
+++ next.config.js
@@ -1,45 +1,19 @@
-const composePlugins = require("next-compose-plugins")
-
-const withPWA = [
-  require("next-pwa"),
-  {
-    pwa: {
-      dest: "public",
-      buildExcludes: [
-        /middleware-manifest\.json$/,
-        /_middleware\.js$/,
-        /middleware-chunks/,
-        /_middleware\.js\.map$/,
-        /middleware-runtime\.js$/,
-        /server\/pages\/_middleware\.js$/,
-      ],
-    },
-  },
-]
-
-const plugins = [require("next-plugin-preact"), withPWA]
-
-const securityHeaders = [
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
-  },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
-  },
-]
+const withPWA = require("next-pwa")
+const withPreact = require("next-plugin-preact")
 
 /** @type {import("next").NextConfig} */
-const nextConfig = {
+let config = {
+  pwa: {
+    dest: "public",
+    buildExcludes: [
+      /middleware-manifest\.json$/,
+      /_middleware\.js$/,
+      /middleware-chunks/,
+      /_middleware\.js\.map$/,
+      /middleware-runtime\.js$/,
+      /server\/pages\/_middleware\.js$/,
+    ],
+  },
   experimental: {
     outputStandalone: true,
   },
@@ -48,14 +22,9 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: securityHeaders,
-      },
-    ]
-  },
 }
 
-module.exports = composePlugins(plugins, nextConfig)
+config = withPWA(config)
+config = withPreact(config)
+
+module.exports = config
