847c56bebed48e89af0e7d0a3ec94f2d4bc81635
Chrisando E. Pramudhita <chrsep@protonmail.com>
2022-03-08 12:44:32 +0700

    extract svg out.

--- src/components/social-login.tsx
+++ src/components/social-login.tsx
@@ -2,16 +2,16 @@ import { FC, ReactNode } from "react"
 
 const SocialLogin: FC<{
   label: string
-  logo: ReactNode
   onClick?: () => void
-}> = ({ onClick, logo, label }) => (
+  logoUrl: string
+}> = ({ onClick, logoUrl, label }) => (
   <div>
     <button
       onClick={onClick}
       className="inline-flex h-full w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
     >
       <span className="sr-only">{label}</span>
-      {logo}
+      <img src={logoUrl} alt="Facebook" className={"h-5 w-5 opacity-60"} />
     </button>
   </div>
 )
